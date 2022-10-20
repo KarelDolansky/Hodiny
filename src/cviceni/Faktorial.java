3ed443816d3c1cd5a6f2c3a0649be904529ba97f
Karel Dolanský <karel.dolansky@tul.cz>
2022-10-20 11:14:46 +0200

    oprava 2

---
+++ src/cviceni/Faktorial.java
@@ -0,0 +1,23 @@
+package cviceni;
+
+import java.util.Scanner;
+
+
+public class Faktorial {
+
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Cislo: ");
+        int n = sc.nextInt();
+        long fak = 1;
+        while (n > 1) {
+            fak = fak * n;
+            --n;
+        }
+        System.out.println(fak);
+    }
+
+}
